01d011a0d89c223f48886dc54e82672be5e227f2
okadareo <okadareo0614@gmail.com>
2020-12-08 06:23:07 +0000

    アプケーションリを完成させよう１　５章

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,24 +1,25 @@
 class BlogsController < ApplicationController
-  
+
   def show
   end
 
   def index
+    @blogs = Blog.all
   end
 
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_params)
-    list.save
+    blog.save
     redirect_to blogs_path
   end
 
   def edit
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
